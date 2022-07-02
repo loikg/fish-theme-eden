a9cced1b97a2c4b1bdc870f07660822cc5db3810
loik <loik@FIX-LOIKG.localdomain>
2022-07-02 20:44:38 +0800

    feat: add cloud profiles in prompt

--- functions/eden_tools_version.fish
+++ functions/eden_tools_version.fish
@@ -1,26 +1,22 @@
-function _node_version
-    echo (command node --version 2> /dev/null)
-end
-
-function _show_node_version -d "Show node version in prompt"
-    if [ (_node_version) ]
-        set -l node_version '(node:'(_node_version)') '
-        echo -ne "$node_version"
+function _azure_subscription
+    if type -q az
+        echo (command az account show --query 'name' 2> /dev/null) | sed 's/"//g'
     end
 end
 
-function _go_version
-    echo (command go version 2> /dev/null)
+function _show_azure_subscription
+    if [ (_azure_subscription) ]
+        echo -ne '(az:'(_azure_subscription)') '
+    end
 end
 
-function _show_go_version -d "Show node version in prompt"
-    if [ (_go_version) ]
-        set -l go_version '('(_go_version)') '
-        echo -ne "$go_version"
+function _show_aws_profile
+    if set -q AWS_PROFILE
+        echo -ne "(aws:$AWS_PROFILE) "
     end
 end
 
 function show_tools_version -d ""
-    _show_node_version
-    # _show_go_version
+    _show_aws_profile
+    _show_azure_subscription
 end
