2fb0a3473157ea52a266064072f793ea73d6431e
loik <loik@FIX-LOIKG.localdomain>
2022-07-02 18:02:28 +0800

    feat: disable right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,13 +1,17 @@
 function fish_right_prompt
 
-  # Last command status
-  set -l code $status
-  if test $code != 0
-    echo -s (set_color red) '-' $code '- '
-  end
+    if test ! "$theme_show_right_prompt" = yes
+        return 0
+    end
 
-  # Timestamp
-  set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
-  echo (date "+%H:%M:%S")
+    # Last command status
+    set -l code $status
+    if test $code != 0
+        echo -s (set_color red) - $code '- '
+    end
+
+    # Timestamp
+    set_color $fish_color_autosuggestion 2>/dev/null; or set_color 555
+    echo (date "+%H:%M:%S")
 
 end
