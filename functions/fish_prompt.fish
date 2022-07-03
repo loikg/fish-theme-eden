19e99deee30b9e7c16283da9c0a4f1f02d58dd92
loik <loik@FIX-LOIKG.localdomain>
2022-07-03 10:50:59 +0800

    fix: command not found

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -89,6 +89,29 @@ function show_prompt_char -d "Terminate with a nice prompt char"
     echo -n -s $normal $THEME_EDEN_PROMPT_CHAR ' '
 end
 
+function _azure_subscription
+    if type -q az
+        echo (command az account show --query 'name' 2> /dev/null) | sed 's/"//g'
+    end
+end
+
+function _show_azure_subscription
+    if [ (_azure_subscription) ]
+        echo -ne '(az:'(_azure_subscription)') '
+    end
+end
+
+function _show_aws_profile
+    if set -q AWS_PROFILE
+        echo -ne "(aws:$AWS_PROFILE) "
+    end
+end
+
+function show_tools_version -d ""
+    _show_aws_profile
+    _show_azure_subscription
+end
+
 function fish_prompt
     set fish_greeting
 
